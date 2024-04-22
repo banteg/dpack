89b5a353966a138694ec54f46c6ca9bf5d6031ae
banteg <4562643+banteg@users.noreply.github.com>
2024-04-22 15:11:55 +0400

    feat: ape dpack dir and repr

--- ape_dpack.py
+++ ape_dpack.py
@@ -23,6 +23,12 @@ class ApeDpack(BaseModel):
     def __getattr__(self, name):
         return self.contract_instances.get(name)
 
+    def __dir__(self):
+        return super().__dir__() + sorted(self.contract_instances.keys())
+
+    def __repr__(self):
+        return f"<ApeDpack contract_types={list(self.contract_types)} contract_instances={list(self.contract_instances)}>"
+
 
 def load(path):
     return ApeDpack(dpack=dpack.load(path))
