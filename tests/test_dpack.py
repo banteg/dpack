7959cb493fcfdef710aca585a9ef12d07c17360e
banteg <4562643+banteg@users.noreply.github.com>
2024-04-22 14:45:50 +0400

    test: pack builder

--- tests/test_dpack.py
+++ tests/test_dpack.py
@@ -48,3 +48,39 @@ def test_getattr(pack):
 def test_dir(pack):
     print(dir(pack))
     assert {"weth", "weth9"} & set(dir(pack))
+
+
+@pytest.mark.parametrize("pack_type", [True, False])
+def test_builder(pack, pack_type):
+    builder = dpack.builder(network="ropsten")
+    if pack_type:
+        builder.pack_type(typename="WETH9", artifact="tests/weth-ropsten-artifact.json")
+    for name in ["weth", "weth9"]:
+        builder.pack_object(
+            typename="WETH9",
+            artifact="tests/weth-ropsten-artifact.json",
+            objectname=name,
+            address="0xc778417E063141139Fce010982780140Aa0cD5Ab",
+        )
+    assert builder.build().model_dump() == pack.model_dump()
+
+
+def test_builder_chaining(pack):
+    built = (
+        dpack.builder(network="ropsten")
+        .pack_type(typename="WETH9", artifact="tests/weth-ropsten-artifact.json")
+        .pack_object(
+            typename="WETH9",
+            artifact="tests/weth-ropsten-artifact.json",
+            objectname="weth",
+            address="0xc778417E063141139Fce010982780140Aa0cD5Ab",
+        )
+        .pack_object(
+            typename="WETH9",
+            artifact="tests/weth-ropsten-artifact.json",
+            objectname="weth9",
+            address="0xc778417E063141139Fce010982780140Aa0cD5Ab",
+        )
+        .build()
+    )
+    assert built.model_dump() == pack.model_dump()
